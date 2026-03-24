98dfe6ad43bd47f379b0aaa71e9dc92504bb58b9
sameerrana7 <113410632+sameerrana7@users.noreply.github.com>
2026-03-24 15:52:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 console.log("PORT VALUE:", process.env.PORT);
 
 // ─── Security Middleware ─────────────────────────────────────────────
-app.use(helmet());
+// app.use(helmet());
 
 app.use(cors({
   origin: [
